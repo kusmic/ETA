067e51ac0a35808d02aa5d446c6aaf732366ee20
kusmic <samirkusmic@gmail.com>
2021-10-02 13:54:43 -0600

    Updated classes.py, still oworking on putting it all together like in main.py

--- classes.py
+++ classes.py
@@ -4,53 +4,78 @@ import astropy.units as u
 import astropy.constants as c
 from astropy.modeling.models import BlackBody1D
 
+# parameters to normalize to Vega
 VEGA_T = 9600 * u.K
 VEGA_V = 5500 * u.AA
 VEGA_BBl = 3.63e-9 * u.erg * u.s**-1 * u.cm**-2 * u.AA**-1
 VEGA_BBn = ((VEGA_V**2/c.c)*VEGA_BBl).to(u.erg * u.s**-1 * u.cm**-2 * u.Hz**-1)
 VEGA_wl_ENG = (c.h * c.c / VEGA_V).to(u.erg)
+
 class Source:
     """
     Defining Source object: it has a flux one can observe from it at a certain
     wavelength, used to derive the total photon flux observed later on.
     """
     
-    def __init__(self, wavelengths, flux_density_lambda = None, T_rad = None, 
-                 BB=True, Vega_norm=False):
+    def __init__(self, wavelengths, mag=None, bands=None, flux_density_lambda=None, 
+                 T_rad = None, BB=True, Vega_norm=False):
         """
-        wavelength: numpy ndarray of wavelength values, should have units.
-        flux density_lambda: Flux density in wavelength units (cgs)
-        T_rad: radiative temperature of source, used to calculate blackbody's
-        flux density when BB is True. Does nothing otherwise. Default is None.
-        BB: Boolean to state if source is a blackbody. Default is True.  
+        -wavelength (float 1darray): numpy ndarray of wavelength values, should have units.
+        -flux density_lambda (float 1darray): Flux density in wavelength units
+        (cgs)
+        -T_rad (float): radiative temperature of source, used to calculate 
+        blackbody's flux density when BB is True. Does nothing otherwise. 
+        Default is None.
+        -BB (bool): Boolean to state if source is a blackbody. Default is True. 
+        -mag (float 1darray, float): apparent magnitude of source if flux 
+        density to not be used. 
+        -bands (string 1darray, string): Bands classifying mag
+        -Vega_norm (bool): Boolean to set whether to normalize flux density to
+        observed Vega values. Default is False.
         
         """
         self.wavelengths = wavelengths.to(u.AA)
-        self.__isBB__ = BB
-        if BB == True:
-            bb_model = BlackBody1D(temperature=T_rad)
-            self.frequencies = self.wavelengths.to(u.Hz, equivalencies=u.spectral())
-            self.F_nu = bb_model(self.wavelengths)
-            self.F_lambda = ((self.frequencies**2/c.c)*self.F_nu).to(u.erg * u.s**-1 * u.cm**-2 * u.AA**-1,
-                            equivalencies=u.spectral())
-            
+        self.frequencies = self.wavelengths.to(u.Hz, equivalencies=u.spectral())
+        # Samir found out you can call class method before its definition
+        if mag==None:
+            self.f_lambda, self.F_lambda, self.f_nu, self.F_nu = self.__setFluxDensities__(
+                    self.wavelengths, self.frequencies, BB, T_rad, Vega_norm)
         else:
-            self.F_lambda = flux_density_lambda.to(u.erg * u.s**-1 * u.cm**-2 * u.AA**-1)
-            self.frequencies = self.wavelengths.to(u.Hz, equivalencies=u.spectral())
-            self.F_nu = ((wavelengths**2/c.c)*(self.F_lambda)).to(u.erg * u.s**-1 * u.cm**-2 * u.Hz**-1)
+            self.mag = mag
+            self.bands = bands
+        self.__isBB__ = BB
+        self.__isVegaNorm__ = Vega_norm        
+        
+    def __setFluxDensities__(self, lam, nu, isBB=False, T_rad=None, VegaNorm=False):
+        if isBB == True:
+            bb_model = BlackBody1D(temperature=T_rad)
+            # energy flux density in frequency
+            F_nu = bb_model(self.wavelengths)
+            # energy flux density in wavelength
+            F_lambda = ((self.frequencies**2/c.c)*F_nu).to(u.erg * u.s**-1 * u.cm**-2 * u.AA**-1,
+                       equivalencies=u.spectral())
+                
+        else:
+            F_lambda = self.flux_density_lambda.to(u.erg * u.s**-1 * u.cm**-2 * u.AA**-1)
+            F_nu = ((self.wavelengths**2/c.c)*(F_lambda)).to(u.erg * u.s**-1 * u.cm**-2 * u.Hz**-1)
             
-        if Vega_norm == True:
+        if VegaNorm == True:
             index_5500 = np.where(self.wavelengths.value==5500)
             flux_5500 = self.F_lambda[index_5500]
-            self.F_lambda = self.F_lambda / flux_5500
-            self.F_lambda = self.F_lambda * VEGA_BBl 
+            F_lambda = self.F_lambda / flux_5500
+            F_lambda = self.F_lambda * VEGA_BBl 
+            F_nu = ((self.wavelengths**2/c.c)*(F_lambda)).to(u.erg * u.s**-1 * u.cm**-2 * u.Hz**-1)
         
-        self.f_nu = self.F_nu / ((c.h*self.frequencies).to(u.erg))
-        self.f_lambda = self.F_lambda / ((c.c * c.h / self.wavelengths).to(u.erg))
+        # photon flux density in frequency
+        f_nu = F_nu / ((c.h*self.frequencies).to(u.erg))
+        # photon flux density in wavelength
+        f_lambda = F_lambda / ((c.c * c.h / self.wavelengths).to(u.erg))
+            
+        return(f_lambda, F_lambda, f_nu, F_nu)
     
-class Atmosphere:
+class Sky:
     
-    def __init__(self, a_lambda):
+    def __init__(self, sky_absorp=None, sky_emiss=None, a_lambda=None):
         
         self.a_lambda = a_lambda
         
@@ -117,7 +142,15 @@ class Instrument:
  
 class Filter:
     
-    def __init__(self, b_lambda):
+    def __init__(self, b_lambda=None), banddata=None, bandname=None):
         
         self.b_lambda = b_lambda
+        
+class Observation:
+    
+    def __init__(self, source, sky, telescope, mirror, instrument, band):
+        pass
+    
+    def __set_source__(self, source, sky, telescope, mirror, instrument, band):
+        pass
   
