ef38774ef0c753571ceaaa7db950348a5c5d3f60
kusmic <samirkusmic@gmail.com>
2021-10-04 22:51:05 -0600

    Made it as truly OOP as I can

--- classes.py
+++ classes.py
@@ -3,6 +3,8 @@ import numpy as np
 import astropy.units as u
 import astropy.constants as c
 from astropy.modeling.models import BlackBody1D
+from astropy.io import fits
+from astropy.table import QTable
 
 # parameters to normalize to Vega
 VEGA_T = 9600 * u.K
@@ -17,7 +19,7 @@ class Source:
     wavelength, used to derive the total photon flux observed later on.
     """
     
-    def __init__(self, wavelengths, mag=None, bands=None, flux_density_lambda=None, 
+    def __init__(self, wavelengths=None, mag=None, bands=None, flux_density_lambda=None, 
                  T_rad = None, BB=True, Vega_norm=False):
         """
         -wavelength (float 1darray): numpy ndarray of wavelength values, should have units.
@@ -34,15 +36,20 @@ class Source:
         observed Vega values. Default is False.
         
         """
-        self.wavelengths = wavelengths.to(u.AA)
-        self.frequencies = self.wavelengths.to(u.Hz, equivalencies=u.spectral())
+        if wavelengths is not None:
+            self.wavelengths = wavelengths.to(u.AA)
+            self.frequencies = self.wavelengths.to(u.Hz, equivalencies=u.spectral())
         # Samir found out you can call class method before its definition
-        if mag==None:
+        if mag is None:
             self.f_lambda, self.F_lambda, self.f_nu, self.F_nu = self.__setFluxDensities__(
                     self.wavelengths, self.frequencies, BB, T_rad, Vega_norm)
+            self.got_fluxdensity = True
+            self.got_mag = False
         else:
             self.mag = mag
             self.bands = bands
+            self.got_mag = True
+            self.got_fluxdensity = False
         self.__isBB__ = BB
         self.__isVegaNorm__ = Vega_norm        
         
@@ -69,22 +76,45 @@ class Source:
         # photon flux density in frequency
         f_nu = F_nu / ((c.h*self.frequencies).to(u.erg))
         # photon flux density in wavelength
-        f_lambda = F_lambda / ((c.c * c.h / self.wavelengths).to(u.erg))
-            
+        f_lambda = F_lambda / ((c.c * c.h / self.wavelengths).to(u.erg)) 
         return(f_lambda, F_lambda, f_nu, F_nu)
     
 class Sky:
-    
-    def __init__(self, sky_absorp=None, sky_emiss=None, a_lambda=None):
         
-        self.a_lambda = a_lambda
+    def __set_skyBackground__(self, emissPath, source, telescope, instrument):
+        skyTab = QTable(fits.open(emissPath)[1].data)
+        lam = (skyTab["lam"]*u.nm).to(u.AA)
+        flux = skyTab["flux"]/u.s/u.m**2/u.micron/u.arcsec**2#ph/s/m2/micron/arcsec2
+        flux = flux.to(1/u.s/u.cm**2/u.AA/u.arcsec**2)
+        skyBG = np.interp(source.wavelengths, lam, flux)
+        B = np.trapz(skyBG, source.wavelengths)
+        return(B)
+        
+    def __set_skyTransmission__(self, source, absorpPath):
+        skyTab = QTable(fits.open(absorpPath)[1].data)
+        lam = (skyTab["lam"]*u.nm).to(u.AA)
+        trans = skyTab["trans"]
+        a = np.interp(source.wavelengths, lam, trans)
+        return(a)
+    
+    def __init__(self, skyPath=None, a_lambda=None, source=None, 
+                 telescope=None, instrument=None, B=None):
+        if skyPath is not None:
+            self.background = self.__set_skyBackground__(skyPath, source, telescope,
+                                                  instrument)
+        else:
+            self.background = B
+        if a_lambda is not None:
+            self.a_lambda = a_lambda
+        elif a_lambda is None and skyPath is not None:
+            self.a_lambda = self.__set_skyTransmission__(source, skyPath)
         
 class Telescope:
     
     def __init__(self, t_lambda, diameter=None, mirrors=None, name=None, eps=0):
         
         self.t_lambda = t_lambda
-        if name == None:
+        if name is None:
             self.diameter=diameter
             self.mirrors=mirrors
             self.eps=eps
@@ -122,11 +152,11 @@ class Instrument:
     describing the instrumental uncertainties'''
     
     def __init__(self, i_lambda, datatype="imaging", dispersion=None, 
-                 sigma_rn = None, pixel_scale = None, name=''):
+                 sigma_rn = None, pixel_scale = None, name='', zpt=None):
         
         self.i_lambda = i_lambda
         self.name=name
-
+        self.zeropoint = zpt
         if datatype == "imaging":
             self.sigma_rn = sigma_rn
         elif datatype == "spectra":
@@ -142,15 +172,78 @@ class Instrument:
  
 class Filter:
     
-    def __init__(self, b_lambda=None), banddata=None, bandname=None):
+    def __init__(self, b_lambda=None, banddata=None, bandname=None):
         
         self.b_lambda = b_lambda
         
 class Observation:
     
-    def __init__(self, source, sky, telescope, mirror, instrument, band):
+    def __init__(self, source, sky, telescope, mirror, instrument, band, aperture):
+        self.aperture_radius = aperture
+        self.source = source
+        self.sky = sky
+        self.telescope = telescope
+        self.mirror = mirror
+        self.instrument = instrument
+        self.band = band
+        self.__SpT__ = self.__set_SpT__()
         pass
     
-    def __set_source__(self, source, sky, telescope, mirror, instrument, band):
-        pass
+    def __set_SpT__(self):
+        if self.source.got_fluxdensity:
+            f = self.source.f_lambda * self.sky.a_lambda * self.telescope.t_lambda * self.mirror.const * self.instrument.i_lambda * self.band.b_lambda
+            SpT = self.telescope.area() * np.trapz(f, self.source.wavelengths)
+        elif self.source.got_mag:
+            SpT = 10**(-0.4*(self.source.mag - self.instrument.zeropoint)) / u.s
+        return(SpT)
+        
+    def get_exposure_time(self, SNR, limit="none"):
+        SpT = self.__SpT__
+        aper_area = self.aperture_radius**2 * np.pi
+        N_pix = np.pi*(self.aperture_radius * self.instrument.pixel_scale)**2
+        Nsig = N_pix * self.instrument.sigma_rn**2
+        ABpT = aper_area * self.sky.background
+        if limit == "signal":
+            t = SNR**2 * SpT / SpT**2
+        elif limit == "bg":
+            t = SNR**2 * ABpT / SpT**2
+        elif limit == "readout":
+            t = SNR*np.sqrt(Nsig) / SpT
+        else:
+            a = SpT**2
+            b = -SNR**2* (SpT + ABpT)
+            c = -SNR**2 * Nsig
+            t = (-b + np.sqrt(b**2 - 4 * a * c)) / (2*a)
+        
+        return t
+    
+    def get_SNR(self, t, limit="none"):
+        """
+        Gets the SNR from given exposure time t and each class.
+        BG: background's surface brightness rate [photons/cm2/s]
+        aperture_radius: radius of seeing area
+        limit: is this statistically limited? It can be signal-, background-, or 
+        readout-limited. Each denoted as "signal", "bg", and "readout". Otherwise,
+        it is "none" and all three are taken into consideration. Default:"none"
+        
+        Out:
+            SNR - float denoting the signal to noise ratio.
+        """
+        
+        S = t * self.__SpT__
+        aper_area = self.aperture_radius**2 * np.pi
+        N_pix = np.pi*(self.aperture_radius * self.instrument.pixel_scale)**2
+        Nsig = N_pix * self.instrument.sigma_rn**2
+        B = aper_area * self.sky.background * t
+        if limit == "signal":
+            SNR = S / np.sqrt(S)
+        elif limit == "bg":
+            SNR = S / np.sqrt(B)
+        elif limit == "readout":
+            SNR = S / np.sqrt(Nsig)
+        else:
+            SNR = S / np.sqrt(S + B + Nsig)
+        
+        return SNR
+    
   
--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import numpy as np
 import classes as cl
 import astropy.units as u
-from routines import get_SNR, get_exposure_time
 
 if __name__ == "__main__":
     dlam = 1
     lam_arr = np.arange(4000, 9000, dlam) * u.AA
-    test_source = cl.Source(lam_arr, BB=True, T_rad = 6000*u.K, Vega_norm=True)
-    test_atmos = cl.Atmosphere(np.ones(lam_arr.size)*0.5)
+    test_BgSB = 10 / u.arcsec**2 / u.s
+
+    test_source = cl.Source(mag=5, bands="V")
+    test_atmos = cl.Sky(a_lambda=np.ones(lam_arr.size)*0.5, B=test_BgSB)
     part_thruput = np.power(0.3, 1/3)
     test_tele = cl.Telescope(part_thruput, diameter=0.6*u.m, mirrors=[part_thruput])
     test_mirror = cl.Mirror(np.ones(lam_arr.size)*part_thruput)
     test_inst = cl.Instrument(np.ones(lam_arr.size)*part_thruput, sigma_rn=10, 
-                              pixel_scale=20 / u.arcsec)
+                              pixel_scale=2 / u.arcsec, zpt=-22)
     test_band = cl.Filter(np.ones(lam_arr.size)*0.8)
-    test_BgSB = 1000 / u.arcsec**2 / u.s
     test_exposure_t = 100 * u.s
     test_aper_radius = 2 * u.arcsec
     test_SNR = 100
     
-    obs_SNR = get_SNR(test_exposure_t, test_BgSB, test_aper_radius, test_source,
-                      test_atmos, test_tele, test_mirror, test_inst, test_band)
-    obs_exp_t = get_exposure_time(test_SNR, test_BgSB, test_aper_radius, test_source,
-                      test_atmos, test_tele, test_mirror, test_inst, test_band)
+    test_obs = cl.Observation(test_source, test_atmos, test_tele, test_mirror,
+                              test_inst, test_band, test_aper_radius)
+    obs_SNR = test_obs.get_SNR(test_exposure_t)
+    obs_exp_t = test_obs.get_exposure_time(test_SNR)
     print("SNR: ", obs_SNR)
     print("Exposure time: ", obs_exp_t)
